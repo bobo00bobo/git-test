8939a7e3b354db9f49de2f9bbe74a2662017398a
bobo <bobo@atguigu.com>
2022-04-20 18:01:24 +0800

    second commit GitTest

--- src/main/java/com/bobo/git/GitTest.java
+++ src/main/java/com/bobo/git/GitTest.java
@@ -7,5 +7,6 @@ package com.bobo.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.print( "hello Git!!!");
+        System.out.print( "hello Git!!!2");
     }
 }
