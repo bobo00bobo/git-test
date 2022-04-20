3ec1e5f1ca802eccdbcf04e56a4155ebffbd3765
bobo <bobo@atguigu.com>
2022-04-20 17:08:24 +0800

    first commit GitTest

---
+++ src/main/java/com/bobo/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.bobo.git;
+
+/**
+ * @author Bobo
+ * @create 2022-04-20 17:03
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.print( "hello Git!!!");
+    }
+}
