b96c7e78f61eb3a76979de67b6c4fa435429700d
bobo <bobo@atguigu.com>
2022-04-20 19:13:02 +0800

    master commit2

--- src/main/java/com/bobo/git/GitTest.java
+++ src/main/java/com/bobo/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.print( "hello Git!!!");
         System.out.print( "hello Git!!!2");
         System.out.print( "hello Git!!!3");
-        System.out.println("hello Git !!!4"); //hot-fix所写
+        System.out.println("hello Git !!!4"); //hot-fix所写,无冲突的合并
+        System.out.println("mater test");
     }
 }
